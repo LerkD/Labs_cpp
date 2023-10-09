a7a9f8f32742543ac943901352f0bb5eb490c165
LerkD <leradulghieri@gmail.com>
2023-10-09 11:25:15 +0300

    First part of Lab3

---
+++ Lab3/main.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+int hello (int a)
+{
+    int res = a + a;
+    return res; 
+}
+
+int main()
+{
+    int result = hello(5);
+
+     std::cout << "Result: " << result << std::endl;
+
+
+    int nextFunction(int b)
+    {
+
+    } 
+
+
+
+    return 0;
+}
