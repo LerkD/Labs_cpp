f8bad46f72ea00d3789eb0a2f671971af342b388
LerkD <leradulghieri@gmail.com>
2023-10-09 12:40:56 +0300

    GlobalVar

---
+++ Lab3/Task2/GlobalVar/f.cpp
@@ -0,0 +1,5 @@
+// Глобальная переменная в f.cpp
+int globalVar = 20;
+
+// Объявление функции hello в f.cpp
+int hello(int num);
---
+++ Lab3/Task2/GlobalVar/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>>
+// Глобальная переменная в main.cpp
+int globalVar = 10;
+
+// Объявление функции hello в main.cpp
+int hello(int a);
+
+int main() {
+    int result = hello(5); // Вызываем функцию hello и сохраняем результат
+    std::cout << "Результат функции hello: " << result << std::endl;
+    return 0;
+}
