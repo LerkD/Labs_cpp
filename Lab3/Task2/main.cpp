cd1c4ae9a62201aca1af9cf631b5d8a0400eb3d6
LerkD <leradulghieri@gmail.com>
2023-10-09 12:29:04 +0300

    Updates

--- Lab3/Task2/main.cpp
+++ Lab3/Task2/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
-// Объявление функции hello в main.cpp
-int hello(int a);
+// Определение функции hello в main.cpp (с модификатором static)
+static int hello(int a)
+{
+    return (a + a);
+};
 
 int main() {
     int result = hello(5); // Вызываем функцию hello и сохраняем результат
