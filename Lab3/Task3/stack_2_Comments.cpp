5b2e754db4bc4a9301cbb2106243a890a8ad9886
LerkD <leradulghieri@gmail.com>
2023-10-09 13:21:28 +0300

    Task3

--- Lab3/Task3/stack_2_Comments.cpp
+++ Lab3/Task3/stack_2_Comments.cpp
@@ -1,41 +1,47 @@
 #include <stdlib.h>
 #include <iostream>
 
-// This program was designed to show you how the stack works.
-// You shouldn't dereference pointers which are no longer in scope.
-// Never do that, it's undefined behavior (anything can happen).
 
-// Note that this program may break if you use optimizations.
-// Also, I have only tried it on Zig C++ (Clang) on Windows.
-
-// Also note, that this produces a warning on Zig C++ (Clang), which is expected.
-
-// See the excel file for a visualization of the stack.
-
-int* stackMemory1()
+int* stackMemory1() //функция, возвращающая указатель на целочисленное значение 
 {
-    int a = 1;
-    return &a;
+    int a = 1; //Объявление и инициализация переменной а со значением 1 
+    return &a; // возвращение указателя на переменную а 
 }
 
-int* stackMemory2()
+int* stackMemory2() //функция, возвращающая указатель на целочисленное значение 
+
 {
-    int b = 2;
-    return &b;
+    int b = 2; //Объявление и инициализация переменной b со значением 2
+    return &b; // возвращение указателя на переменную b
 }
 
 int main()
 {
-    int* b = stackMemory1();
-    int a1 = *b; // 1
-    int* c = stackMemory2();
-    int a2 = *c; // 2
+    int* b = stackMemory1(); //Вызов функции stackMemory1 и сохранение возвращенного указателя в b
+    int a1 = *b; // 1, 
+
+    /*В данной строке происходит разыменование указателя b, то есть получение значения, на которое указывает указатель b. 
+    В данном случае, b содержит адрес переменной a, которая была объявлена внутри функции stackMemory1(). 
+    После разыменования, значение, на которое указывает b, присваивается переменной a1, то есть 1*/
+
+    int* c = stackMemory2(); //Вызов функции stackMemory2 и сохранение возвращенного указателя в c
+    int a2 = *c; // 2, 
+
+    /*В данной строке происходит разыменование указателя с, то есть получение значения, на которое указывает указатель с. 
+    В данном случае, c содержит адрес переменной b, которая была объявлена внутри функции stackMemory1(). 
+    После разыменования, значение, на которое указывает c, присваивается переменной a2, то есть 2*/
+
     int a3 = *b; // 2
 
-    std::cout << a1 << std::endl; // prints 1
-    std::cout << a2 << std::endl; // prints 2
-    std::cout << a3 << std::endl; // prints 2
-    std::cout << ((std::byte*) b - (std::byte*) c) << std::endl; // prints 0, pointers are equal
+    /*В данной строке снова происходит разыменование указателя b, то есть получение значения, на которое указывает указатель b. 
+    Однако, сейчас b указывает на переменную b, объявленную в stackMemory2(), так как она была перезаписана в предыдущей строке. 
+    Поэтому после разыменования a3 получает значение переменной b, которая была установлена равной 2 внутри функции stackMemory2(). 
+    Таким образом, a3 содержит значение 2, а не 1, как a1*/
+
+    std::cout << a1 << std::endl; //Выводим значение а1, то есть prints 1
+    std::cout << a2 << std::endl; //Выводим значение а1, то есть prints 2
+    std::cout << a3 << std::endl; //Выводим значение а1, то есть prints 2
+    std::cout << ((std::byte*) b - (std::byte*) c) << std::endl; //Выводим разницы между указателями b и c в байтах (0, указатели равны)
 
     return 0;
 }
