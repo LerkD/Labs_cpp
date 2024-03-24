1da7ebcb882833decd712fb3bf9fe9da6d8f860c
LerkD <leradulghieri@gmail.com>
2024-03-24 19:35:38 +0200

    Different configurations and sizes

--- ASDC_Labs/Lab7 (ASDC)/Quick_Sort.cpp
+++ ASDC_Labs/Lab7 (ASDC)/Quick_Sort.cpp
@@ -1,16 +1,7 @@
 #include "header.h"
 
-int main()
+void runSortAlgorithm(std::span<Person> people_span)
 {
-    std::array<Person, 5> people = {
-        {{"Lera", 12, Gender::Girl},
-         {"Anton", 9, Gender::Boy},
-         {"Dima", 7, Gender::Boy},
-         {"Catea", 8, Gender::Girl},
-         {"Asea", 3, Gender::Girl}}};
-
-    std::span<Person> people_span(people);
-
     std::cout << "Initial array:" << std::endl;
     printPeople(people_span);
     std::cout << std::endl;
@@ -32,8 +23,61 @@ int main()
     std::cout << "Quick Sort Swaps: " << swaps << std::endl;
     std::cout << "Quick Sort Time: " << elapsed_seconds_quick.count() << "s\n";
     std::cout << std::endl;
+}
 
+int main()
+{
+    std::array<Person, 5> people = {
+        {{"Lera", 12, Gender::Girl},
+         {"Anton", 9, Gender::Boy},
+         {"Dima", 7, Gender::Boy},
+         {"Catea", 8, Gender::Girl},
+         {"Asea", 3, Gender::Girl}}};
+
+    runSortAlgorithm(people);
+
+    std::cout << "------------\n" << std::endl;
+
+    // Другая конфигурация 
+    std::array<Person, 5> people2 = {
+        {{"Anton", 9, Gender::Boy},
+         {"Lera", 12, Gender::Girl},
+         {"Catea", 8, Gender::Girl},
+         {"Asea", 3, Gender::Girl},
+         {"Dima", 7, Gender::Boy}}};
+
+    runSortAlgorithm(people2);
+
+    std::cout << "------------\n" << std::endl;
+
+    // Другая конфигурация 
+    std::array<Person, 5> people3 = {
+        {{"Asea", 3, Gender::Girl},
+         {"Anton", 9, Gender::Boy},
+         {"Dima", 7, Gender::Boy},
+         {"Catea", 8, Gender::Girl},
+         {"Lera", 12, Gender::Girl}}};
+
+    runSortAlgorithm(people3);
+
+    std::cout << "------------\n" << std::endl;
+    std::cout << "Different size: \n" << std::endl;
+
+    std::array<Person, 12> people_long = {
+        {{"Lera", 12, Gender::Girl},
+         {"Anton", 9, Gender::Boy},
+         {"Dima", 7, Gender::Boy},
+         {"Catea", 8, Gender::Girl},
+         {"Asea", 3, Gender::Girl},
+         {"Masha", 1, Gender::Girl},
+         {"Vasea", 5, Gender::Boy},
+         {"Danil", 2, Gender::Boy},
+         {"Anea", 4, Gender::Girl},
+         {"Sonya", 11, Gender::Girl},
+         {"Misha", 10, Gender::Boy},
+         {"Olea", 11, Gender::Girl}}};
+
+    runSortAlgorithm(people_long);
 
     return 0;
 }
-
--- ASDC_Labs/Lab7 (ASDC)/Shell_Sort.cpp
+++ ASDC_Labs/Lab7 (ASDC)/Shell_Sort.cpp
@@ -1,23 +1,14 @@
 #include "header.h"
 
-int main()
+void runSortAlgorithm(std::span<Person> people_span)
 {
-    std::array<Person, 5> people = {
-        {{"Lera", 12, Gender::Girl},
-         {"Anton", 9, Gender::Boy},
-         {"Dima", 7, Gender::Boy},
-         {"Catea", 8, Gender::Girl},
-         {"Asea", 3, Gender::Girl}}};
-
-    std::span<Person> people_span(people);
-
     std::cout << "Initial array:" << std::endl;
     printPeople(people_span);
     std::cout << std::endl;
-    
+
     int comparisons = 0, swaps = 0;
 
-   // Запускаем и измеряем время для Shell Sort
+    // Запускаем и измеряем время для Shell Sort
     auto start_shell = std::chrono::steady_clock::now();
     shellSort(people_span, comparisons, swaps);
     auto end_shell = std::chrono::steady_clock::now();
@@ -32,6 +23,61 @@ int main()
     std::cout << "Shell Sort Swaps: " << swaps << std::endl;
     std::cout << "Shell Sort Time: " << elapsed_seconds_shell.count() << "s\n";
     std::cout << std::endl;
+}
+
+int main()
+{
+    std::array<Person, 5> people = {
+        {{"Lera", 12, Gender::Girl},
+         {"Anton", 9, Gender::Boy},
+         {"Dima", 7, Gender::Boy},
+         {"Catea", 8, Gender::Girl},
+         {"Asea", 3, Gender::Girl}}};
+
+    runSortAlgorithm(people);
+
+    std::cout << "------------\n" << std::endl;
+
+    // Другая конфигурация 
+    std::array<Person, 5> people2 = {
+        {{"Anton", 9, Gender::Boy},
+         {"Lera", 12, Gender::Girl},
+         {"Catea", 8, Gender::Girl},
+         {"Asea", 3, Gender::Girl},
+         {"Dima", 7, Gender::Boy}}};
+
+    runSortAlgorithm(people2);
+
+    std::cout << "------------\n" << std::endl;
+
+    // Другая конфигурация 
+    std::array<Person, 5> people3 = {
+        {{"Asea", 3, Gender::Girl},
+         {"Anton", 9, Gender::Boy},
+         {"Dima", 7, Gender::Boy},
+         {"Catea", 8, Gender::Girl},
+         {"Lera", 12, Gender::Girl}}};
+
+    runSortAlgorithm(people3);
+
+    std::cout << "------------\n" << std::endl;
+    std::cout << "Different size: \n" << std::endl;
+
+    std::array<Person, 12> people_long = {
+        {{"Lera", 12, Gender::Girl},
+         {"Anton", 9, Gender::Boy},
+         {"Dima", 7, Gender::Boy},
+         {"Catea", 8, Gender::Girl},
+         {"Asea", 3, Gender::Girl},
+         {"Masha", 1, Gender::Girl},
+         {"Vasea", 5, Gender::Boy},
+         {"Danil", 2, Gender::Boy},
+         {"Anea", 4, Gender::Girl},
+         {"Sonya", 11, Gender::Girl},
+         {"Misha", 10, Gender::Boy},
+         {"Olea", 11, Gender::Girl}}};
+
+    runSortAlgorithm(people_long);
 
     return 0;
 }
